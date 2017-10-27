26b779e0699508b06ddf16629cabf4a3459be8fc
franklang <lang.frank79@gmail.com>
2017-10-27 08:09:22 +0200

    Create PageDynamic-ContentBasis.all.all.php

---
+++ external/override/modules/website/templates/PageDynamic-ContentBasis.all.all.php
@@ -0,0 +1,25 @@
+<?php
+// "$this" is an instance of website_Page
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" ?>
+<?php echo $this->getDoctype(); ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:change="http://www.rbs.fr/change/1.0/schema" xml:lang="<?php echo $this->getLang(); ?>" lang="<?php echo $this->getLang(); ?>" class="no-js">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title><?php echo htmlspecialchars($this->getTitle(), ENT_COMPAT, "utf-8"); ?></title>
+    <meta http-equiv="content-language" content="<?php echo $this->getLang(); ?>" />
+    <meta name="language" content="<?php echo $this->getLang(); ?>" />
+    <meta name="description" <?php echo f_util_HtmlUtils::buildAttribute("content", $this->getDescription()); ?> />
+    <meta name="keywords" <?php echo f_util_HtmlUtils::buildAttribute("content", $this->getKeywords()); ?> />
+    <meta name="generator" content="RBS Change <?php echo Framework::getVersion(); ?>" />
+    <?php echo $this->getMetas(); ?>
+    <script type="text/javascript">var pageHandler = <?php echo $this->getJSONHandler(); ?></script>
+    <?php echo $this->getPlainHeadMarker(); ?>
+    <?php echo $this->getStylesheetInclusions(); ?>
+    <?php echo $this->getStyles(); ?>
+    <?php echo $this->getScripts(); ?>
+    <?php echo $this->getLinkTags(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+    <?php echo $this->htmlBody; ?>
+  <?php $this->renderBenchTimes(); ?>
+</html>
